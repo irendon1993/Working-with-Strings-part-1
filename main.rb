83bff68c846aef7d9041fb25394fede70231ad3b
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-03-30 16:51:04 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,6 @@
+#String concatenation
+first_name = "Isaac "
+last_name = "Rendon"
+puts first_name + last_name
+#String Interpolation
+puts "My first name is #{first_name} and my last name is #{last_name}"
